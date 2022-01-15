5c774f8a9cf3a94badbfd7d8109a691bd98d017b
xieyimaple <1067545977@qq.com>
2022-01-15 13:53:40 +0800

    完善observable功能开发

--- example/src/observable/lib/http.ts
+++ example/src/observable/lib/http.ts
@@ -1,8 +1,96 @@
 /**
  * @module chat
  * @description 此模块为所有类的基类，维护所有类通用的公共属性，公共方法。
-*/
+ */
 
-import { EventEmitter } from "react-native";
+import { YCObject } from './base';
 
-export class YCHttp extends EventEmitter {}
+export enum YCHttpInterfaceEnum {
+	getVersion = '/kkrp/app_version/find', // 获取版本
+	login = '/kkrp/member/login', // 登录
+	logout = '/kkrp/member/logout', //注销
+	registerInfo = '/kkrp/member/fields', // 获取注册所需信息
+	register = '/kkrp/member/register', // 注册
+	updatePassword = '/kkrp/member/reset_old_pwd', // 换密码
+	forgotPassword = '/kkrp/member/reset_pwd', // 忘记密码
+	resetPwd = '/kkrp/member/reset_pwd_sms', // 重置密码
+	queryUserInfo = '/kkrp/member/info', // 查询用户信息
+	registerVerification = '/kkrp/member/regPwd', // 注册验证
+	registerSMS = '/kkrp/member/reg_sms', // 注册短信
+	addFriend = '/kkrp/member/friend/apply_msg', // 添加好友
+	getFriendInfoByAccount = '/kkrp/member/friend/find_friend', // 根据账号搜索好友
+	queryAllFriend = '/kkrp/member/friend/list', // 查询所有好友
+	quitGroupChat = '/kkrp/im_group/group_quit', // 退出群聊
+	createGroupChat = '/kkrp/im_group/create_group' // 创建群聊
+}
+
+class YCHttp extends YCObject {
+	private _baseUrl = 'http://api.efhgrihvwe-ioh3dmqgt6.com';
+	private _token = '';
+	private _lastResponse = '';
+
+	get baseUrl(): string {
+		return this._baseUrl;
+	}
+
+	set baseUrl(value: string) {
+		this._baseUrl = value;
+	}
+
+	get token(): string {
+		return this._token;
+	}
+
+	set token(value: string) {
+		this._token = value;
+	}
+
+	// 最后一次的请求返回头
+	get lastResponse(): string {
+		return this._lastResponse;
+	}
+
+	public async post(path: YCHttpInterfaceEnum, content: Record<string, string>, headers?: Headers | string[][] | { [key: string]: string }, queryParams?: string) {
+		this.verifyParams();
+		try {
+			const result = await fetch(`${this.baseUrl}${path}${queryParams}`, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					...headers
+				},
+				body: JSON.stringify(content)
+			});
+			this._lastResponse = JSON.stringify(result);
+			return result;
+		} catch (error) {
+			console.log(`Post ${this.baseUrl}${path} Error`);
+			console.log(error);
+			throw error;
+		}
+	}
+
+	public async get(path: YCHttpInterfaceEnum, queryParams?: string) {
+		this.verifyParams();
+		try {
+			const result = await fetch(`${this.baseUrl}${path}${queryParams}`);
+			this._lastResponse = JSON.stringify(result);
+			return result;
+		} catch (error) {
+			console.log(`Get ${this.baseUrl}${path} Error`);
+			console.log(error);
+			throw error;
+		}
+	}
+
+	protected verifyParams(): boolean {
+		// 相关的前置验证
+		if (!this._baseUrl) {
+			throw new Error('缺少关键参数baseUrl');
+		}
+		return true;
+	}
+}
+
+export const chatHttp = new YCHttp();
--- example/src/observable/lib/user.ts
+++ example/src/observable/lib/user.ts
@@ -1,126 +1,129 @@
 /**
  * @module user
  * @description 此模块为用户相关类，主要功能为用户信息的获取（用户名、头像、好友列表，与好友建立长连接通信等等）
-*/
-import { EventEmitter } from "react-native";
-import { YCFriend } from "./friend";
+ */
+import { YCObject } from '..';
+import { YCChat } from './chat';
+import { YCFriend } from './friend';
 
-type YCUserInfo = {
-    name: string;
-    id?: string;
-    password?: string;
-    photoUrl?: string;
-    photoPath?: string;
-    logined?: boolean;
-};
+/**
+ * 用户信息接口
+ */
+export interface YCUserInfo {
+	name: string;
+	id?: string;
+	password?: string;
+	photoUrl?: string;
+	photoPath?: string;
+	logined?: boolean;
+}
 
 /**
  *  @class User
  *  @desc 用户基类，该类的作用为实现用户相关功能以及属性。
  * */
-export class YCUser extends EventEmitter {
-    private _id?: string;
-    private _name: string;
-    private _logined = false;
-    private _password?: string;
-    private _photoUrl?: string;
-    private _photoPath?: string;
+export class YCUser extends YCObject implements YCUserInfo {
+	private _id: string;
+	private _name: string;
+	private _logined = false;
+	private _password: string;
+	private _photoUrl: string;
+	private _photoPath: string;
 
-    private _currentChattingFriend: YCFriend;
-    private _chatList: YCFriend[];
+	private _currentChattingFriend: YCFriend;
+	private _chatList: YCFriend[];
 
-    // get 访问器：获取id
-    get id(): string {
-        return this._id;
-    };
+	// get 访问器：获取id
+	get id(): string {
+		return this._id;
+	}
 
-    // get 访问器：获取用户名
-    get name(): string {
-        return this._name;
-    };
+	// get 访问器：获取用户名
+	get name(): string {
+		return this._name;
+	}
 
-    // get 访问器：获取密码
-    get password(): string {
-        return this._password;
-    };
+	// get 访问器：获取密码
+	get password(): string {
+		return this._password;
+	}
 
-    // get 访问器：获取头像URL
-    get photoUrl(): string {
-        return this._photoUrl;
-    };
+	// get 访问器：获取头像URL
+	get photoUrl(): string {
+		return this._photoUrl;
+	}
 
-    // get 访问器：获取头像路径
-    get photoPath(): string {
-        return this._photoPath;
-    };
+	// get 访问器：获取头像路径
+	get photoPath(): string {
+		return this._photoPath;
+	}
 
-    // get 访问器：获取头像
-    get photo(): string {
-        return this.photoPath || this.photoUrl || '默认头像';
-    };
+	// get 访问器：获取头像
+	get photo(): string {
+		return this.photoPath || this.photoUrl || '默认头像';
+	}
 
-    // get 访问器：获取当前用户的激活状态（即是否登录）
-    get logined(): boolean {
-        return this._logined;
-    };
+	// get 访问器：获取当前用户的激活状态（即是否登录）
+	get logined(): boolean {
+		return this._logined;
+	}
 
-    // get 访问器：获取当前正在聊天的
-    get currentChattingFriend(): YCFriend {
-        return this._currentChattingFriend;
-    };
+	// get 访问器：获取当前正在聊天的
+	get currentChattingFriend(): YCFriend {
+		return this._currentChattingFriend;
+	}
 
-    // get 访问器：获取用户名
-    set currentChattingFriend(value: YCFriend) {
-        this._currentChattingFriend = value;
-    };
+	// get 访问器：获取用户名
+	set currentChattingFriend(value: YCFriend) {
+		this._currentChattingFriend = value;
+	}
 
-    // get 访问器：获取用户的聊天列表，用以显示界面上
-    get chatList(): YCFriend[] {
-        return this._chatList;
-    }
+	// get 访问器：获取用户的聊天列表，用以显示界面上
+	get chatList(): YCFriend[] {
+		return this._chatList;
+	}
 
-    // 构造函数
-    constructor({
-        id = '',
-        name,
-        password = '',
-        logined = false,
-        photoUrl = '',
-        photoPath = '',
-    }: YCUserInfo) {
-        super();
-        this._id = id;
-        this._name = name;
-        this._password = password;
-        this._logined = logined;
-        this._photoUrl = photoUrl;
-        this._photoPath = photoPath;
-    }
+	// 构造函数
+	constructor(owner: YCChat, { id = '', name, password = '', logined = false, photoUrl = '', photoPath = '' }: YCUserInfo) {
+		super();
+		this._id = id;
+		this._name = name;
+		this._password = password;
+		this._logined = logined;
+		this._photoUrl = photoUrl;
+		this._photoPath = photoPath;
+	}
 
+	// 将用户信息转为JSON格式，用于进程间通信或者数据发送
+	public toJson(): string {
+		return JSON.stringify({
+			id: this.id,
+			name: this.name,
+			photo: this.photo
+		});
+	}
 
-    // 将用户信息转为JSON格式，用于进程间通信或者数据发送
-    public toJson(): string {
-        return JSON.stringify({
-            id: this.id,
-            name: this.name,
-            photo: this.photo
-        });
-    };
+	// 静态方法：通过json对象构造一个user对象
+	static fromJson(owner: YCChat, json: string) {
+		const jsonObj: { id: string; name: string } = JSON.parse(json);
+		if (jsonObj.name || jsonObj.id) {
+			throw Error('缺少关键参数id，name');
+		}
+		const user = new YCUser(owner, jsonObj);
+		user._id = jsonObj.id;
+		return user;
+	}
 
-    // 静态方法：通过json对象构造一个user对象
-    static fromJson(json: string) {
-        const jsonObj: { id: string; name: string } = JSON.parse(json);
-        if (jsonObj.name || jsonObj.id) {
-            throw Error('缺少关键参数id，name');
-        }
-        const user = new YCUser(jsonObj);
-        user['_id'] = jsonObj.id;
-        return user;
-    }
+	// 删除好友
+	public deleteFriend() {}
 
-    // 删除聊天列表中的某个好友记录
-    public deleteFriendFromChatList() {}
+	// 添加好友
+	public addFriend() {}
 
-    // 删除好友
-    public deleteFriend() {}
+	// 创建群聊
+	public createGroupChat() {
+		// 1. 创建群组
+
+		// 2. 创建群组会话（用以在界面显示，群组会话保存在owner的conversationList属性上）
+	}
 }
