3d241b742b54886ebf023604a56648ccc97db1cd
tangongliang <543110872@qq.com>
2022-01-15 18:09:18 +0800

    代码上库

--- example/src/examples/CustomExpression.js
+++ example/src/examples/CustomExpression.js
@@ -11,7 +11,7 @@ export default class CustomAlertDialog extends Component {
         };
     }
 
-    componentWillReceiveProps(nextProps) {
+    UNSAFE_componentWillReceiveProps(nextProps) {
         this.setState({isVisible: nextProps.show});
     }
 
